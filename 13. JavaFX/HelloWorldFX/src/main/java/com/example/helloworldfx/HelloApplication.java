f1543221f1a282983ee69eb8af05988af1f9f44f
Florian-Cristinel Ianculescu <florian.ianculescu@gmail.com>
2023-03-07 19:54:27 +0200

    Add files via upload

---
+++ 13. JavaFX/HelloWorldFX/src/main/java/com/example/helloworldfx/HelloApplication.java
@@ -0,0 +1,34 @@
+package com.example.helloworldfx;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+
+//        GridPane root = new GridPane();
+//        root.setAlignment(Pos.CENTER);
+//        root.setVgap(10);
+//        root.setHgap(10);
+        Scene scene = new Scene(fxmlLoader.load(), 700, 275);
+
+//        Label greeting = new Label("Welcome to JavaFX!");
+//        greeting.setTextFill(Color.GREEN);
+//        greeting.setFont(Font.font("Times New Roman", FontWeight.BOLD, 70));
+//        root.getChildren().add(greeting);
+
+        stage.setTitle("Hello JavaFX!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
